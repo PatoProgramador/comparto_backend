bbbe0cf059c49459e76c2033dc7ae1af6bc6bf2e
Angel <apuentest@uniempresarial.edu.co>
2025-06-26 19:38:27 -0500

    Add DonacionEntity mapping

--- src/infraestructure/config/data-base.ts
+++ src/infraestructure/config/data-base.ts
@@ -1,29 +1,30 @@
-import { DataSource } from 'typeorm';
-import { User } from '../entities/User';
-import dotenv from 'dotenv';
+import { DataSource } from "typeorm";
+import { User } from "../entities/User";
+import { DonacionEntity } from "../entities/Donacion";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
+  type: "postgres",
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5432'),
+  port: parseInt(process.env.DB_PORT || "5432"),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  schema:"public",
-  entities: [User],
+  schema: "public",
+  entities: [User, DonacionEntity],
   synchronize: true,
   logging: true,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
 
 export const initDB = async () => {
   try {
     await AppDataSource.initialize();
-    console.log('Database connected');
+    console.log("Database connected");
   } catch (error) {
     console.error("Error connecting to the database:", error);
     process.exit(1);

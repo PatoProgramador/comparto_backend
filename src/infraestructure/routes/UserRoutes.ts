2d171515465ebc5bf233c128518d261ba8cd5186
Angel <apuentest@uniempresarial.edu.co>
2025-06-26 19:39:10 -0500

    Improve format

--- src/infraestructure/routes/UserRoutes.ts
+++ src/infraestructure/routes/UserRoutes.ts
@@ -3,52 +3,52 @@ import { UserAdapter } from "../adapter/UserAdapter";
 import { UserApplicationService } from "../../application/UserApplicationService";
 import { UserController } from "../controller/UserController";
 
-const router =Router();
+const router = Router();
 
 const userAdapter = new UserAdapter();
 const userAppServise = new UserApplicationService(userAdapter);
 const userController = new UserController(userAppServise);
 
-router.get("/users",async (req, res)=>{
+router.get("/users", async (req, res) => {
   try {
-    await userController.getAllUsers(req,res);
+    await userController.getAllUsers(req, res);
   } catch (error) {
     res.status(400).json({
       message: "Error fetching users",
-      error
+      error,
     });
   }
 });
 
-router.get("/users/:id", async(req,res) =>{
-  try{
-    await userController.getUserById(req,res);
+router.get("/users/:id", async (req, res) => {
+  try {
+    await userController.getUserById(req, res);
   } catch (error) {
     res.status(400).json({
       message: "Error fetching user by ID",
-      error
+      error,
     });
   }
 });
 
-router.patch("/users/:id", async(req,res) =>{
-  try{
-    await userController.updateUser(req,res);
+router.patch("/users/:id", async (req, res) => {
+  try {
+    await userController.updateUser(req, res);
   } catch (error) {
     res.status(400).json({
       message: "Error fetching user by ID",
-      error
+      error,
     });
   }
 });
 
-router.delete("/user/:id", async(req, res) =>{
+router.delete("/user/:id", async (req, res) => {
   try {
     await userController.deleteUser(req, res);
   } catch (error) {
     res.status(400).json({
       message: "Error deleting user",
-      error
+      error,
     });
   }
 });
